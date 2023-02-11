a1b0d9121df8b706cfb8bad3ab3517666126ca79
salma77 <salmaabdelfatah77@gmail.com>
2023-02-11 20:57:09 +0200

    added walls, still a bit buggy

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,23 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './Node.css';
+import "./Node.css";
 
 export default class Node extends Component {
-    constructor(props){
-      super(props);
-      this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   render() {
-    const {isStart, isFinish} = this.props;
-    const extraClassName = isFinish? 'node-finish':isStart? 'node-start': '';
+    const {
+      col,
+      isFinish,
+      isStart,
+      isVisited,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+        ? "node-start"
+        : isVisited ? "node-visited" : isWall ? "node-wall" : "";
     return (
-      <div className={`node ${extraClassName}`}></div>
-    )
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      />
+    );
   }
 }
-
-export const DEFAULT_NODE = {
-  row: 0,
-  col: 0,
-};
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { djikstra, getNodesInShortestPathOrder } from "../algorithms/djikstra";
 import Node from './Node/Node';
 
 import './PathfindingVisualizer.css';
@@ -9,10 +10,11 @@ const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;  
 
 export default class PathfindingVisualizer extends Component {
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state = {
-            grid: []
+            grid: [],
+            mousePressed: false,
         };
     }
 
@@ -21,20 +23,58 @@ export default class PathfindingVisualizer extends Component {
         this.setState({grid})
     }
 
+    handleMouseDown(row, col){
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col)
+        this.setState({grid: newGrid, mousePressed: true})
+    }
+
+    handleMouseEnter(row, col){
+        if(!this.state.mousePressed) return 
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col)
+        this.setState({grid: newGrid})
+    }
+
+    handleMouseUp(){
+        this.setState({mousePressed:false});
+    }
+
+    animateDjikstra(visitedNodesInOrder){
+        for(let i = 0; i < visitedNodesInOrder.length; i++){
+            setTimeout(()=>{
+                const node = visitedNodesInOrder[i]
+                const newGrid = this.state.grid.slice()
+                const newNode = {
+                    ...node,
+                    isVisited: true,
+                };
+                newGrid[node.row][node.col] = newNode
+                this.setState({grid: newGrid});
+            }, 15 * i);
+        }
+    }
+
+    visualizeDjikstra(){
+        const {grid} = this.state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL]
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL]
+        const visitedNodesInOrder = djikstra(grid, startNode, finishNode)
+        this.animateDjikstra(visitedNodesInOrder)
+    }
+
     render(){
         const {grid} = this.state;
 
         return(
             <>
                 <button onClick={() => this.visualizeDjikstra()}>
-                Vizzzz!!!
+                Visualize Djikstra Now
                 </button>
                 <div className="grid">
                     {grid.map((row, rowIdx) => {
                         return (
                             <div key={rowIdx}>
                                 {row.map((node, nodeIdx) => {
-                                    const {row, col, isFinish, isStart, isWall} = node;
+                                    const {row, col, isFinish, isStart, isVisited, isWall} = node;
                                     return(
                                         <Node 
                                             key={nodeIdx} 
@@ -42,6 +82,7 @@ export default class PathfindingVisualizer extends Component {
                                             row={row}
                                             isStart={isStart}
                                             isFinish={isFinish}
+                                            isVisited={isVisited}
                                             isWall={isWall}></Node>
                                     )}
                                 )}
@@ -77,4 +118,15 @@ const createNode = (col, row) => {
         isWall: false,
         previousNode: null,
     }
+}
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice()
+    const node = newGrid[row][col]
+    const newNode = {
+        ...node,
+        isWall: !node.isWall,
+    }
+    newGrid[row][col] = newNode
+    return newGrid;
 }
